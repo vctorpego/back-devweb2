096ec9d5af9ff298e0a51d531360cbd9c03375f1
Eduarda <gavaeduarda@gmail.com>
2025-11-06 13:07:10 -0300

    Correcao de erros

--- videolocadora/videolocadora/src/main/java/com/locadora/videolocadora/dtos/ItemRecordDto.java
+++ videolocadora/videolocadora/src/main/java/com/locadora/videolocadora/dtos/ItemRecordDto.java
@@ -1,11 +1,15 @@
 package com.locadora.videolocadora.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.locadora.videolocadora.models.ItemModel;
 import java.time.LocalDate;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record ItemRecordDto(
         Long id,
         String numeroSerie,
+        @JsonFormat(pattern = "yyyy-MM-dd")
         LocalDate dataAquisicao,
         ItemModel.TipoItem tipo,
         Long tituloId,
